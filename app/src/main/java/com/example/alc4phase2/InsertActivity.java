e8ebd4311c5d77f8eb1da9a159d36fda1ebc862c
Daniel <oluwabukunmi.aluko@gmail.com>
2019-08-01 02:21:06 +0100

    .

--- app/src/main/java/com/example/alc4phase2/InsertActivity.java
+++ app/src/main/java/com/example/alc4phase2/InsertActivity.java
@@ -1,13 +1,13 @@
 package com.example.alc4phase2;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InsertActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_insert);
     }
 }
